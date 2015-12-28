a041fa6323c1489e49545ec286b115b213c8c7fc
RyodoTanaka <k104073r@mail.kyutech.jp>
2015-12-28 22:26:19 +0900

    [modify] cleanコマンドの追加・findコマンドの変更

--- build.sh
+++ build.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
+find_files=$(find . -name "build.cmake")
+command=$(echo ${find_files} | sed -e "s/ /\n/g" | sed -e "s/\.\//cmake -P \.\//g")
 
-find_files=$(find .. -name "build.cmake")
-command=$(echo ${find_files} | sed -e "s/ /\n/g" | sed -e "s/\.\.\//cmake -P \.\.\//g")
+if [ -z "$1" ]; then
+	echo "Start to build !!"
+	${command}
+	echo "Compiled List" 
+	echo ${find_files} | sed -e "s/ /\n/g"	
+elif [ $1 = "clean" ]; then
+	remove_files=$(find . -name "build")
+	rm -rf ${remove_files}
+	echo "Removed"
+	echo ${remove_files}
+else
+	echo "Uncorrect command..."
+fi
 
-echo "Start to build !!"
-${command}
-echo "Complete to build that ..." 
-echo ${find_files} | sed -e "s/ /\n/g"
+
+
