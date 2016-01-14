be0103e313cc8b727e0318c0b5113140bde5f672
RyodoTanaka <k104073r@mail.kyutech.jp>
2016-01-14 16:58:53 +0900

    [add] 漸化式のDPを追加

--- Algorithm/DP/napsac/loop/main.cpp
+++ Algorithm/DP/napsac/loop/main.cpp
@@ -9,34 +9,37 @@ int n = 4;			// 商品の合計個数
 int W = 5;			// 商品の合計価値
 vector<int> w, v;	// w : 重さ, v : 価値
 
-// i番目までで重さがu以下の価値の最大値を返す
-int search(int i, int u)
-{
-  if(i == n) // 合計個数に到達していたら終了
-	return 0;
-  else if(u < w[i]) // uより重さが重たくなったらi番目のを使用しない
-	return search(i+1, u);
-  else{
-	int ret1 = search(i+1, u);				// i番目を使用しない場合
-	int ret2 = search(i+1, u-w[i]) + v[i];	// i番目を使用する場合
-	return max(ret1, ret2);                 // 価値の高いほうを返す
-  }
-}
+vector< vector<int> > dp(n+1, vector<int>(W+1,0));  // DPの表(0で初期化)
 
+int cnt = 0; // ループカウンタ
 
 int main(int argc, char* argv[])
 {
+  // 初期代入
   w.push_back(2);
   w.push_back(1);
   w.push_back(3);
   w.push_back(2);
+  w.push_back(0);
 
   v.push_back(3);
   v.push_back(2);
   v.push_back(4);
   v.push_back(2);
-   
-  cout << search(0, W) << endl;
+  v.push_back(0);
+
+  for(int i=n-1; i>=0; i--)
+	for(int u=1; u<=W; u++){
+	  cnt++;
+	  if(u < w[i])
+		dp[i][u] = dp[i+1][u];
+	  else
+		dp[i][u] = max(dp[i+1][u], dp[i+1][u-w[i]]+v[i]);
+	}
+
+  cout << dp[0][W] << endl;
+  cout << cnt << endl;
+  
     
   return 0;
 }
