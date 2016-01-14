4721d7cbc2b3199c71a72520cc381e4f48847454
RyodoTanaka <k104073r@mail.kyutech.jp>
2016-01-14 13:45:40 +0900

    [modify] ファイル整理を行った

--- IO/src/main.cpp
+++ IO/src/main.cpp
@@ -5,23 +5,12 @@
 using namespace std;
 
 int main(int argc, char *argv[])
-{
-  string filename;
-  cout << "Input File name" << endl;
-  cin >> filename;
-  
-  // Open the file
-  ifstream fin(filename.c_str());
-  if(!fin){
-    cerr << "Cannot open the file." << endl;
-    exit(1);
-  }
-  
+{  
   // Read the file. And find ";".
   char ch;
   int cnt = 0;
-  while(!fin.eof()){
-    fin >> ch;
+  while(!cin.eof()){
+    cin >> ch;
     if(ch==';')
       cnt++;
   }
