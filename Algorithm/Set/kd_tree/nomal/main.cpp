448f346808e38fbab64ac94885d800fca120de58
RyodoTanaka <k104073r@mail.kyutech.jp>
2016-04-21 22:12:45 +0900

    [add] 二次元のkd_treeを追加
    完全ではないが、一旦commit
    ゆくゆくはきちんとライブラリを使いたい

---
+++ Algorithm/Set/kd_tree/nomal/main.cpp
@@ -0,0 +1,65 @@
+#include <iostream>
+
+using namespace std;
+
+struct point {
+  int x;
+  int y;
+};
+
+struct node {
+  point val;
+  node *lch, *rch;
+};
+
+bool x_or_y = false; // true : x, false : y
+
+node *insert(node *p, point data)
+{
+  if(p == NULL){ // if the node is empty.
+	node *q = new node;
+	q->val = data;
+	q->lch = q->rch = NULL;
+	return q;
+  }
+  else{ // if the node is not empty
+	if(x_or_y){ // use x value
+	  if(data.x < p->val.x)
+		p->lch = insert(p->lch, data);
+	  else
+		p->rch = insert(p->rch, data);
+	  x_or_y = !x_or_y;
+	  return p;
+	} else {
+	  if(data.y < p->val.y)
+		p->lch = insert(p->lch, data);
+	  else
+		p->rch = insert(p->rch, data);
+	  x_or_y = !x_or_y;
+	  return p;
+	}
+  }
+}
+
+int main(int argc, char* argv[])
+{
+  node *root = NULL;
+  point tmp;
+
+  tmp.x = 2;
+  tmp.y = 5;
+  root = insert(root, tmp);
+  tmp.x = 6;
+  tmp.y = 3;
+  root = insert(root, tmp);
+  tmp.x = 3;
+  tmp.y = 8;
+  root = insert(root, tmp);
+  tmp.x = 8;
+  tmp.y = 9;
+  root = insert(root, tmp);
+
+  cout << "(" << root->rch->val.x << "," << root->rch->val.y << ")" << endl;
+
+  return 0;
+}
