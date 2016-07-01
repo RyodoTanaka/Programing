fea27681f473a9a61c045880b1c54714caa86d45
RyodoTanaka <k104073r@mail.kyutech.jp>
2016-07-01 21:06:13 +0900

    [add] kd_tree2次元版を追加

--- Algorithm/Set/kd_tree/nomal/main.cpp
+++ Algorithm/Set/kd_tree/nomal/main.cpp
@@ -10,9 +10,10 @@ struct point {
 struct node {
   point val;
   node *lch, *rch;
+  bool x_or_y; // true : x, false : y
 };
 
-bool x_or_y = false; // true : x, false : y
+bool tmp = false;
 
 node *insert(node *p, point data)
 {
@@ -20,22 +21,22 @@ node *insert(node *p, point data)
 	node *q = new node;
 	q->val = data;
 	q->lch = q->rch = NULL;
+	q->x_or_y = tmp;
+	tmp = !tmp;
 	return q;
   }
   else{ // if the node is not empty
-	if(x_or_y){ // use x value
+	if(p->x_or_y){ // use x value
 	  if(data.x < p->val.x)
 		p->lch = insert(p->lch, data);
 	  else
 		p->rch = insert(p->rch, data);
-	  x_or_y = !x_or_y;
 	  return p;
 	} else {
 	  if(data.y < p->val.y)
 		p->lch = insert(p->lch, data);
 	  else
 		p->rch = insert(p->rch, data);
-	  x_or_y = !x_or_y;
 	  return p;
 	}
   }
@@ -45,21 +46,22 @@ int main(int argc, char* argv[])
 {
   node *root = NULL;
   point tmp;
-
+  
   tmp.x = 2;
   tmp.y = 5;
   root = insert(root, tmp);
   tmp.x = 6;
   tmp.y = 3;
   root = insert(root, tmp);
-  tmp.x = 3;
-  tmp.y = 8;
+  tmp.x = 10;
+  tmp.y = 3;
   root = insert(root, tmp);
   tmp.x = 8;
   tmp.y = 9;
   root = insert(root, tmp);
 
   cout << "(" << root->rch->val.x << "," << root->rch->val.y << ")" << endl;
+  cout << root->rch->x_or_y << endl;
 
   return 0;
 }
