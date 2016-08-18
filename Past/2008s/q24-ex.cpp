56fd5d49c82f0de16a082493be1f7471e6dac3b2
Ryodo Tanaka <groadpg@gmail.com>
2016-08-18 17:06:46 +0900

    [add] q24の辺ではなく，ノード数最小を求めるプログラム

---
+++ Past/2008s/q24-ex.cpp
@@ -0,0 +1,112 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <fstream>
+#include <sstream>
+#include <string>
+
+using namespace std;
+
+#define V_MAX 100
+#define READ_NUM 181
+
+int main(int argc, char** argv)
+{
+
+  bool flg = true;
+  for(int v_max=2; flg; v_max++){
+	vector<int> G[v_max]; // 隣接リスト
+	vector<int> G_deg(v_max); // 次数
+  
+	ifstream ifs(argv[1], ios::in);
+	string buf;
+	int cnt = 0;
+	// = file 読み込み ==========================================
+	while(1){
+	  // read by line
+	  getline(ifs, buf);
+	  cnt++;
+	  if(cnt > 181)
+		break;	
+	  // read by delimiter on reading "one" line
+	  const char delimiter = ' ';	
+	  string spreat_buf;
+	  int edge[2];
+	  istringstream line_separater(buf);
+	  for(int i=0; ;i++){
+		if(line_separater.eof())
+		  break;
+		getline(line_separater, spreat_buf, delimiter);
+		edge[i] = atoi(spreat_buf.c_str());
+	  }
+
+	  // 無向グラフの隣接リストを作成
+	  if(edge[0] <= v_max && edge[1] <= v_max){
+		G[edge[0]-1].push_back(edge[1]-1);
+		G[edge[1]-1].push_back(edge[0]-1);
+	  }
+	}
+	// = file 読み込み ========================================
+
+	// 無向グラフの中身を確認
+	vector<int>::iterator itr;
+	for(int i=0; i<v_max; i++){
+	  // 各隣接リストをソート
+	  sort(G[i].begin(),G[i].end()); // 昇順
+	  //sort(G[i].begin(), G[i].end(), greater<int>()); //降順
+
+	  // 隣接リストのサイズを保存
+	  G_deg[i] = G[i].size();
+
+	  // 隣接リストの表示
+	  if(G[i].size()){
+	  	cout << "G[" << i << "] : ";
+	  	for(itr=G[i].begin(); itr<G[i].end(); itr++)	
+	  	  cout << *itr << ", ";
+	  	cout << endl;	  
+	  }
+	  
+	}
+
+	// すべてのクラスタ係数を求める
+	vector<double> cluster(v_max); // クラスタ係数
+	double sum = 0;
+	double k = 0;
+	double tmp = 0;
+	for(size_t i=0; i<cluster.size(); i++){
+	  for(itr=G[i].begin(); itr<G[i].end(); itr++){
+		vector<int>::iterator itr2;
+		for(itr2=G[i].begin(); itr2<G[i].end(); itr2++){
+		  if(find(G[*itr].begin(),G[*itr].end(), *itr2) != G[*itr].end()){
+			flg = false;
+			sum++;
+		  }
+		}
+	  }
+	  sum /= 2.;
+	  k = G[i].size();
+	  if(k<=1)
+		cluster[i] = 0.;
+	  else{
+		tmp = (k*(k-1))/2.;
+		cluster[i] = sum/tmp;
+	  }
+	  sum = 0;
+	  k = 0;
+	  tmp = 0;
+	}
+	// クラスタ係数の表示
+	if(flg==false){
+	  for(size_t i=0; i<cluster.size(); i++)
+		cout << cluster[i] << endl;
+	}
+
+	// 平均クラスタ係数を求める
+	double avg_cluster = accumulate(cluster.begin(),cluster.end(),0.0)/(double)cluster.size();
+	cout << "Avg : " << avg_cluster << endl;
+	cout << v_max << endl;
+  }
+
+  
+  return 0;
+}
