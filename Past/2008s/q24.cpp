369746a614e846a6b250346b7ef1ad883433fb29
Ryodo Tanaka <groadpg@gmail.com>
2016-08-18 21:05:14 +0900

    [add] q2-4解答を追加

--- Past/2008s/q24.cpp
+++ Past/2008s/q24.cpp
@@ -7,110 +7,140 @@
 
 using namespace std;
 
-#define V_MAX 100
-#define READ_NUM 181
+#define V 100
 
+void getGraph(char* filename, int start, int goal, vector< vector<int> >&G, vector<int> &G_deg)
+{
+  ifstream ifs(filename, ios::in);
+  string buf;
+  int cnt = 0;
+  // = file 読み込み ==========================================
+  // start行まで読み飛ばす
+  for(int i=1; i<start; i++)
+	getline(ifs,buf);
+  while(1){
+	// read by line
+	getline(ifs,buf);
+	cnt++;
+	if(cnt > goal-start+1)
+	  break;	
+	// read by delimiter on reading "one" line
+	const char delimiter = ' ';	
+	string spreat_buf;
+	int edge[2];
+	istringstream line_separater(buf);
+	for(int i=0; ;i++){
+	  if(line_separater.eof())
+		break;
+	  getline(line_separater, spreat_buf, delimiter);
+	  edge[i] = atoi(spreat_buf.c_str());
+	}
 
+	// 無向グラフの隣接リストを作成
+	G[edge[0]-1].push_back(edge[1]-1);
+	G[edge[1]-1].push_back(edge[0]-1);
+  }
+  // = file 読み込み ========================================
+
+  // = リストの整理 ===================================
+  vector<int>::iterator itr;
+  for(int i=0; i<G.size(); i++){
+	// == 各隣接リストをソート ==
+	sort(G[i].begin(),G[i].end()); // 昇順
+	//sort(G[i].begin(), G[i].end(), greater<int>()); //降順	  
+  }
+  // = リストの整理 ====================================
+  
+}
+
+bool getCluster(vector<double> &cluster, vector< vector<int> > G)
+{
+  // == すべてのクラスタ係数を求め，連結成分の有無を調べる======================
+  double sum = 0;    // 隣接頂点同士の隣接数の2倍の値
+  double k = 0;      // 次数
+  double tmp = 0;    // sum/2.取りうる値の最大値
+  bool flg = false;  // クラスタ係数0以外があったか(連結成分があったか)
+
+  vector<int>::iterator itr;
+  vector<int>::iterator itr2;
+  
+  for(size_t i=0; i<G.size(); i++){
+	for(itr=G[i].begin(); itr<G[i].end(); itr++){
+	  for(itr2=G[i].begin(); itr2<G[i].end(); itr2++){
+		if(find(G[*itr].begin(),G[*itr].end(), *itr2) != G[*itr].end()){
+		  flg = true; // 連結があった
+		  sum++;
+		}
+	  }
+	}
+	sum /= 2.;
+	k = G[i].size();
+	if(k<=1)
+	  cluster[i] = 0.;
+	else{
+	  tmp = (k*(k-1))/2.;
+	  cluster[i] = sum/tmp;
+	}
+	sum = 0;
+	k = 0;
+	tmp = 0;
+  }
+
+  return flg;
+}
+
+struct binder{
+  int start;
+  int goal;
+  int E;
+  double avg_cluster;
+};
+
+// for print
+ostream& operator<<(ostream& os, const binder& b)
+{
+  os << b.start << ", " << b.goal << ", " << b.E << ", " << b.avg_cluster;
+  return os;
+}
 
 int main(int argc, char** argv)
 {
+  vector< vector<int> > G;  // 隣接リスト
+  G.resize(V);          
+  vector<int> G_deg(V);     // 隣接リストの要素数
+  int start = 1;			// 開始行数(1行目のカウントは１)
+  int goal = 181;			// 終了行数
 
-  double avg_cluster;
-  int start;
-  int cnt = 0;
+
+  vector<double> cluster(V); // クラスタ係数
+  binder smallest;           // 最小エッジ数の情報
+  int e = 1;                 // エッジ数カウンタ
+  smallest.E = 4950;         // 最大エッジ数で初期化
   
-  for(start=2; ;start++){
-    bool flg = true;
-	for(int i=0; flg; i++){
-	  vector<int> G[V_MAX]; // 隣接リスト
-	  vector<int> G_deg(V_MAX); // 次数
-  
-	  ifstream ifs(argv[1], ios::in);
-	  string buf;
-	  cnt = 0;
-	  // = file 読み込み ==========================================
-	  // start行まで読み飛ばす
-	  for(int i=1; i<start; i++)
-		getline(ifs,buf);
-	  while(1){
-		// read by line
-		getline(ifs,buf);
-		cnt++;
-		if(cnt > i)
-		  break;	
-		// read by delimiter on reading "one" line
-		const char delimiter = ' ';	
-		string spreat_buf;
-		int edge[2];
-		istringstream line_separater(buf);
-		for(int i=0; ;i++){
-		  if(line_separater.eof())
-			break;
-		  getline(line_separater, spreat_buf, delimiter);
-		  edge[i] = atoi(spreat_buf.c_str());
-		}
-
-		// 無向グラフの隣接リストを作成
-		G[edge[0]-1].push_back(edge[1]-1);
-		G[edge[1]-1].push_back(edge[0]-1);
+  // 枝切り法で調べる
+  // Q2の問題だから、181行目までなのかな...?
+  // そうでなくとも、forを4950にしてしまえばOK
+  for(int s=1; s+e-1<=181; s++){
+	e=1;
+	for(; e<smallest.E; e++){
+	  getGraph(argv[1], s, s+e-1, G, G_deg);
+	  if(getCluster(cluster, G)){
+		smallest.start = s;
+		smallest.goal = s+e-1;
+		smallest.E = e;
+		smallest.avg_cluster =  accumulate(cluster.begin(),cluster.end(),0.0)/(double)cluster.size();
+		break;
 	  }
-	  // = file 読み込み ========================================
-
-	  vector<int>::iterator itr;
-	  for(int i=0; i<V_MAX; i++){
-		// 各隣接リストをソート
-		sort(G[i].begin(),G[i].end()); // 昇順
-		//sort(G[i].begin(), G[i].end(), greater<int>()); //降順
-
-		// 隣接リストの表示
-		// if(G[i].size()){
-		// 	cout << "G[" << i << "] : ";
-		// 	for(itr=G[i].begin(); itr<G[i].end(); itr++)	
-		// 	  cout << *itr << ", ";
-		// 	cout << endl;	  
-		// }
-	  
-	  }
-
-	  // == すべてのクラスタ係数を求め，連結成分の有無を調べる======================
-	  vector<double> cluster(V_MAX); // クラスタ係数
-	  double sum = 0;
-	  double k = 0;
-	  double tmp = 0;
-	  for(size_t i=0; i<cluster.size(); i++){
-		for(itr=G[i].begin(); itr<G[i].end(); itr++){
-		  vector<int>::iterator itr2;
-		  for(itr2=G[i].begin(); itr2<G[i].end(); itr2++){
-			if(find(G[*itr].begin(),G[*itr].end(), *itr2) != G[*itr].end()){
-			  flg = false;
-			  sum++;
-			}
-		  }
-		}
-		sum /= 2.;
-		k = G[i].size();
-		if(k<=1)
-		  cluster[i] = 0.;
-		else{
-		  tmp = (k*(k-1))/2.;
-		  cluster[i] = sum/tmp;
-		}
-		sum = 0;
-		k = 0;
-		tmp = 0;
-	  }
-	  // クラスタ係数の表示
-	  // if(flg==false){
-	  //   for(size_t i=0; i<cluster.size(); i++)
-	  // 	cout << cluster[i] << endl;
-	  // }
-
-	  // 平均クラスタ係数を求める
-	  avg_cluster = accumulate(cluster.begin(),cluster.end(),0.0)/(double)cluster.size();
-	  if(!flg)
-		cout << "cnt : " << cnt << ", " << "Avg : " << avg_cluster << endl;
 	}
+	G.clear();
+	G.resize(V);
+	G_deg.clear();
+	G_deg.resize(V);
+	cluster.clear();
+	cluster.resize(V);
   }
+
+  cout << smallest << endl;
   
   return 0;
 }
