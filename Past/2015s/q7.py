db3d11fb8e7c21d8029825be828cb6eb9c54ccad
RyodoTanaka <groadpg@gmail.com>
2017-01-30 18:59:57 +0900

    [fix] q7

--- Past/2015s/q7.py
+++ Past/2015s/q7.py
@@ -13,12 +13,15 @@ if __name__ == '__main__':
         if line==past_line:
             print_str.append(line)
         else:
-            del print_str[:]
-            
-        if len(print_str)==3:
-            if print_str[0] not in printed_list:
-                for i in range(len(print_str)+1):
-                    print print_str[0]
+            if len(print_str) > 0:
                 printed_list.append(print_str[0])
+            del print_str[:]
+        if len(print_str)>=3:
+            if print_str[0] not in printed_list:
+                if len(print_str)==3:
+                    for i in range(4):
+                        print print_str[0]
+                else:
+                    print print_str[0]
         past_line=line
     f.close()
