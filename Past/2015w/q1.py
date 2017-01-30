611bce37f720d12580a40493c8db7b583c9697fb
RyodoTanaka <groadpg@gmail.com>
2017-01-30 21:22:32 +0900

    [modify] q1のreturnを修正

--- Past/2015w/q1.py
+++ Past/2015w/q1.py
@@ -8,7 +8,7 @@ def fnc(n):
     if n<1:
         return 1
     else:
-        return (161*fnc(n-1)+2457)%2**24
+        return (161*fnc(n-1)+2457)%(2**24)
 
 if __name__ == '__main__':
     print fnc(int(sys.argv[1]))
