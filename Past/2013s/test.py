b9825d1de02d778c857ac6075b63f0f4208e30d9
RyodoTanaka <groadpg@gmail.com>
2017-02-08 23:14:40 +0900

    [add] test code

---
+++ Past/2013s/test.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import math
+import sys
+
+
+if __name__ == '__main__':
+    i=0
+    while i<10:
+        print i
+        if i==5:
+            i+=1
+        i+=1
+    a=3
+    a+=-4
+    print a
