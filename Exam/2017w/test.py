0b4e50499984fb607d4bb305d0d09c27c952b261
RyodoTanaka <groadpg@gmail.com>
2017-02-09 10:34:13 +0900

    [add] q1

---
+++ Exam/2017w/test.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import math
+import sys
+
+if __name__ == '__main__':
+    print ord(" ")
+    print ord("A")
+    print ord("Z")
+    print ord("a")
+    print ord("z")
